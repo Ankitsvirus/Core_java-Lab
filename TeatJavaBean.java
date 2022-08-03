b23dceab518c62539170f38e40b9684c3b462cda
Ankit Shukla <107361896+Ankitsvirus@users.noreply.github.com>
2022-08-03 15:37:08 +0530

    Add files via upload

---
+++ TeatJavaBean.java
@@ -0,0 +1,13 @@
+package java_beans;
+
+public  class TeatJavaBean {
+	public static void main(String[] args) {
+		Student s =new Student();
+		s.setId(22);
+		System.out.println(s.getId());
+		s.setName("Ankit Shukla");
+		System.out.println(s.getName());
+		
+	}
+
+}
