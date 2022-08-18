e65b042d2eebb41c91a553820aa8184f11978bea
Ankit Shukla <107361896+Ankitsvirus@users.noreply.github.com>
2022-08-18 14:32:44 +0530

    Add files via upload

--- TreeSet1.java
+++ TreeSet1.java
@@ -1,19 +1,5 @@
-package Collection;
-import java.util.*;
+package labtest2;
+
 public class TreeSet1 {
-	   
-	 public static void main(String args[]){  
-	  //Creating and adding elements  
-	  TreeSet<String> ts=new TreeSet<String>();  
-	  ts.add("Ankit");  
-	  ts.add("Rajan");  
-	  ts.add("Anchal");  
-	  ts.add("saurabh");  
-	  //Traversing elements  
-	  Iterator<String> itr=ts.iterator();  
-	  while(itr.hasNext()){  
-	   System.out.println(itr.next());  
-	  }  
-	 }  
-	 
+
 }
