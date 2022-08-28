2e7bd8203e090d0c0d16cf166b4f7a96f4dc60e4
Ankit Shukla <107361896+Ankitsvirus@users.noreply.github.com>
2022-08-28 11:53:07 +0530

    Add files via upload

---
+++ ConsolePassword.java
@@ -0,0 +1,10 @@
+import java.io.Console;  
+class ConsolePassword{    
+public static void main(String args[]){    
+Console c=System.console();    
+System.out.println("Enter password: ");    
+char[] ch=c.ConsolePassword();    
+String pass=String.valueOf(ch);//converting char array into string    
+System.out.println("Password is: "+pass);    
+}    
+}  
